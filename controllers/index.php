cf80729f1f027e724cf74990d232c078b8d679e6
Uroš Jovović <uros.ivo.jovovic@gmail.com>
2024-04-11 19:00:53 +0200

    a

---
+++ controllers/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$title = "";
+$meta_description = "";
+$meta_title = "";
+$og_title = "";
+$og_description = "";
+$og_url = "";
+$og_image = "";
+$og_image_alt = "";
+$preload_cover_image = "";
+
+require "views/index.view.php";
---
+++ views/partials/head.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="sr-RS">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <meta name="description" content="Gladan si? Poruči iz picerije Favola i dobijaš dostavu u 35 minuta! Uživaj u bajkovitim zalogajima pica, tortilja, palačinki i mnogih drugih proizvoda.">
+    <meta name="title" content="Favola Pizza - Neka svaki zalogaj bude bajka!">
+    <meta property="og:type" content="website">
+<meta property="og:title" content="Favola Pizza - Neka svaki zalogaj bude bajka!">
+<meta property="og:url" content="https://www.favola.rs">
+<meta property="og:image" content="https://www.favola.rs/images/cover.webp">
+<meta property="og:description" content="Gladan si? Poruči iz picerije Favola i dobijaš dostavu u 35 minuta! Uživaj u bajkovitim zalogajima pica, tortilja, palačinki i mnogih drugih proizvoda.">
+<meta property="og:image:alt" content="Razna hrana">
+<link rel="preload" as="image" href="/images/cover.webp"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.v2.css">
+    <link rel="icon" href="images/logo.svg">
+    <link rel="icon" type="image/ico" sizes="16x16" rel="noopener" target="_blank" href="images/favicon-16x16.ico">
+<link rel="icon" type="image/ico" sizes="32x32" rel="noopener" target="_blank" href="images/favicon-32x32.ico">
+<link rel="icon" type="image/ico" sizes="any" rel="noopener" target="_blank" href="images/favicon-32x32.ico">
+    <link rel=”mask-icon” href=images/logo.svg” color="#000000">
+    <link rel="apple-touch-icon" href="images/logo.svg">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="manifest" href="manifest.json">
+    <link href="assets/fontawesome-free-6-5-2-web/css/fontawesome.min.css" rel="stylesheet" />
+    <link href="assets/fontawesome-free-6-5-2-web/css/brands.min.css" rel="stylesheet" />
+    <link href="assets/fontawesome-free-6-5-2-web/css/solid.min.css" rel="stylesheet" />
+    <!-- OVO IZBRISATI KADA KRENE LIVE -->
+    <meta name="robots" content="noindex">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
