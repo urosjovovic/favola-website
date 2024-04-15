ed006e63593b8b4981d9d85183ef7426b4641113
Uroš Jovović <uros.ivo.jovovic@gmail.com>
2024-04-15 14:43:31 +0200

    a

--- views/partials/head.php
+++ views/partials/head.php
@@ -21,6 +21,7 @@
 												<?= $og_image_alt ?>">
     <link rel="preload" as="image" href="
 													<?= $preload_cover_image ?>" />
+    <link rel="preload" as="script" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
     <link rel="preload" as="style" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" onload="this.rel='stylesheet'" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="preload" as="style" type="text/css" href="main.v4.css" onload="this.rel='stylesheet'">
     <link rel="icon" href="images/logo.svg">
