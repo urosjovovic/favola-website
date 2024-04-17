559686248e5259c07af35abc031ce6625fed1e5f
Uroš Jovović <uros.ivo.jovovic@gmail.com>
2024-04-17 15:42:17 +0200

    Update head.php

--- views/partials/head.php
+++ views/partials/head.php
@@ -46,8 +46,8 @@
     <!-- <meta name="robots" content="noindex"> -->
     <link rel="preconnect" href="https://consent.cookiebot.com/">
     <link rel="preconnect" href="https://www.googletagmanager.com">
-    <link rel="dns-prefetch" href="https://consent.cookiebot.com/" />.
-    <link rel="dns-prefetch" href="https://www.googletagmanager.com" />.
+    <link rel="dns-prefetch" href="https://consent.cookiebot.com/" />
+    <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
   </head>
   <body>
     <!-- Google Tag Manager (noscript) -->
