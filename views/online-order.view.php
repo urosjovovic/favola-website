4226493a2192e7a360b0e3ddcbaebd0f9faca4d7
Uroš Jovović <uros.ivo.jovovic@gmail.com>
2024-04-17 15:18:42 +0200

    Update online-order.view.php

--- views/online-order.view.php
+++ views/online-order.view.php
@@ -5,7 +5,7 @@ require 'views/partials/header.php';
 
 ?>
 <!-- Page content start -->
-<h1> STIŽE USKORO! </h1>
+<h1 class="text-center py-5"> STIŽE USKORO! </h1>
 <!-- iframe od Mister D -->
 <!-- Page content end -->
 
