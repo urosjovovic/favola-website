3bbde2f9a9c679c94ce2bd172721c3dc6d50db6d
Uroš Jovović <uros.ivo.jovovic@gmail.com>
2024-04-17 15:40:25 +0200

    Update header.php

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,10 +1,11 @@
 <nav class="container-fluid navbar navbar-expand-md navbar-light bg-white p-0 invisible navbar-content-pusher">
   <div class="container p-0">
-    <a class="navbar-brand p-0 pt-1 ms-3 ms-lg-0" href="/">
+    <a class="navbar-brand p-0 pt-1 ms-3 ms-lg-0" href="/#">
+      <!-- <img loading="lazy" alt="Picerija Favola Logo" src="images/favola-header-logo.png"> -->
       <img src="images/logo.svg" alt="Picerija Favola Logo" />
     </a>
-    <h1 class="d-block d-md-none">Pizza Favola</h1>
-    <button class="navbar-toggler me-3 me-lg-0" type="button" data-bs-toggle="collapse" data-bs-target="" aria-controls="" aria-expanded="false" aria-label="Toggle navigation">
+    <h1 class="d-block d-md-none me-3 mb-0">Pizerija Favola</h1>
+    <button class="navbar-toggler me-3 me-lg-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -16,16 +17,16 @@
           <a class="nav-link" href="/#tortilje">Tortilje</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/#piroske-i-obroci">Piroške i Obroci</a>
+          <a class="nav-link" href="/#piroske-i-obroci">Piroške i obroci</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/#sendvici">Sendviči</a>
+          <a class="nav-link" href="/#sendvici">Sendvici</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/#palacinke-slane">Palačinke Slane</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/#palacinke-slatke">Palacinke slatke</a>
+          <a class="nav-link" href="/#palacinke-slatke">Palačinke Slatke</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/#dodaci">Dodaci</a>
@@ -34,17 +35,18 @@
           <a class="nav-link" href="/#pica">Piće</a>
         </li>
         <li class="nav-item free-dostava-item">
-          <a class="nav-link free-dostava-btn d-md-block d-none" href="/#zone">DOSTAVA</a>
+          <a class="nav-link free-dostava-btn d-md-block d-none py-1" href="#zone">DOSTAVA</a>
         </li>
+        <div class="d-md-none py-2 border-info border-top">
+          <span>Telefon: <a class="ms-auto" href="tel:+381628950833">+38162 895 0833</a>
+          </span>
+          <p class="radno-vreme mb-0">Radno vreme - Pon-Sub: 12:00-03:30h</p>
+          <p class="radno-vreme mb-0">Adresa: Ljubićka 11, Beograd</p>
+        </div>
       </ul>
       <div class="call-to-action d-none d-md-block">
         <a class="btn ms-auto" href="tel:+381628950833">+38162 895 0833</a>
         <span class="radno-vreme">Pon-Sub: 12:00-03:30h</span>
-      </div>
-      <div class="d-md-none">
-        <a class="ms-auto" href="tel:+381628950833">Telefon: +38162 895 0833</a>
-        <span class="radno-vreme">Radno vreme - Pon-Sub: 12:00-03:30h</span>
-        <span class="radno-vreme">Adresa: Ljubićka 11, Beograd</span>
       </div>
     </div>
   </div>
