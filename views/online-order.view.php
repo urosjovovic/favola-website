4ea7660041da29b70c4950f491b67e24bfebd4db
Uroš Jovović <uros.ivo.jovovic@gmail.com>
2024-04-17 17:09:14 +0200

    Update online-order.view.php

--- views/online-order.view.php
+++ views/online-order.view.php
@@ -8,7 +8,11 @@ require 'views/partials/header.php';
 <iframe src="https://whitelabel.misterd.rs/favola-dostava?pp=%22www.favola.rs/obavestenje-o-privatnosti-online-porudzbine%22&tos=%22www.favola.rs/uslovi-koriscenja-online-porudzbine%22" 
 style="padding-top:10vh; height:70vh; width: 100vw!important;"></iframe>
 <!-- Page content end -->
-
+<style>
+    .navbar {
+        position: relative!important;
+    }
+</style>
 <?php
 require 'views/partials/footer.php';
 ?>
