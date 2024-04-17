14e92966b9a6292a0e8122bf73b23d316061a279
Uroš Jovović <uros.ivo.jovovic@gmail.com>
2024-04-17 15:24:31 +0200

    Update head.php

--- views/partials/head.php
+++ views/partials/head.php
@@ -44,6 +44,10 @@
     <link rel="preload" as="style" href="assets/fontawesome-free-6-5-2-web/css/solid.min.css" onload="this.rel='stylesheet'"/>
     <!-- OVO IZBRISATI KADA KRENE LIVE -->
     <!-- <meta name="robots" content="noindex"> -->
+    <link rel="preconnect" href="https://consent.cookiebot.com/">
+    <link rel="preconnect" href="https://www.googletagmanager.com">
+    <link rel="dns-prefetch" href="https://consent.cookiebot.com/" />.
+    <link rel="dns-prefetch" href="https://www.googletagmanager.com" />.
   </head>
   <body>
     <!-- Google Tag Manager (noscript) -->
