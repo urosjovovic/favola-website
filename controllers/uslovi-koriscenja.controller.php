a5f05d1d6848d9566bb305400399b20f174a25f9
Uroš Jovović <uros.ivo.jovovic@gmail.com>
2024-05-03 19:29:35 +0200

    init

---
+++ controllers/uslovi-koriscenja.controller.php
@@ -0,0 +1,14 @@
+<?php
+
+$title = "Favola Pizza - Uslovi Korišćenja";
+$meta_description = "Uslovi korišćenja ketering firme pizzerija Favola";
+$meta_title = "Favola Pizza - Uslovi Korišćenja";
+$og_title = "Favola Pizza - Uslovi Korišćenja";
+$og_url = "https://www.favola.rs/uslovi-koriscenja";
+$og_image = "https://www.favola.rs/images/cover-w1250.webpp";
+$og_description = "Uslovi korišćenja ketering firme pizzerija Favola";
+$og_image_alt = "Razna hrana";
+$preload_cover_image = "/images/cover.webp";
+$footer_title_html_raw = "<h2>Favola Pizza</h2>";
+
+require "views/uslovi-koriscenja.view.php";
